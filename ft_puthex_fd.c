105a10486883ebc4619c7540796d981235b2a107
rbilim <ruveydabilimm@gmail.com>
2025-06-27 18:51:21 +0300

    V3

--- ft_puthex_fd.c
+++ ft_puthex_fd.c
@@ -6,12 +6,11 @@
 /*   By: rbilim <rbilim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 19:22:48 by rbilim            #+#    #+#             */
-/*   Updated: 2025/06/27 14:26:47 by rbilim           ###   ########.fr       */
+/*   Updated: 2025/06/27 18:50:03 by rbilim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 void	ft_puthex_fd(long n, int fd, int *len, char format)
 {
@@ -22,12 +21,8 @@ void	ft_puthex_fd(long n, int fd, int *len, char format)
 	else
 		hex = "0123456789abcdef";
 	if (n < 0)
-	{
-		n += 4294967295;
-	}
+		n += 4294967296;
 	if (n >= 16)
-	{
 		ft_puthex_fd(n / 16, fd, len, format);
-	}
 	ft_putchar_fd(hex[n % 16], fd, len);
 }
--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main()
 	int a = 0;
  	//ft_printf("%d\n",ft_printf("%%%%%%%s\n%d\n%", 1, 42));
 	//ft_printf("%d\n",ft_printf("%%%%%%%aaa", 42, 7));
-	ft_printf("%d\n", ft_printf("%4a%\n",42, 7));
-	printf("%d\n", printf("%4a%\n",42, 7));
+	ft_printf("%x\n", -1);
+	printf("%x\n", -1);
 }
