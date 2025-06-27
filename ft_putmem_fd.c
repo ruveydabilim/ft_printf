5c38ef4ed27cc9a079325a0774293ba57b14a702
rbilim <ruveydabilimm@gmail.com>
2025-06-27 19:28:04 +0300

    V4

--- ft_putmem_fd.c
+++ ft_putmem_fd.c
@@ -6,11 +6,21 @@
 /*   By: rbilim <rbilim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 14:13:43 by rbilim            #+#    #+#             */
-/*   Updated: 2025/06/27 15:20:43 by rbilim           ###   ########.fr       */
+/*   Updated: 2025/06/27 19:26:38 by rbilim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void	ft_hex(unsigned long n, int fd, int *len)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	if (n >= 16)
+		ft_hex(n / 16, fd, len);
+	ft_putchar_fd(hex[n % 16], fd, len);
+}
 
 void	ft_putmem_fd(void *ptr, int fd, int *len)
 {
@@ -23,5 +33,5 @@ void	ft_putmem_fd(void *ptr, int fd, int *len)
 		return ;
 	}
 	ft_putstr_fd("0x", fd, len);
-	ft_puthex_fd(addr, fd, len, 'x');
+	ft_hex(addr, fd, len);
 }
--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include "ft_printf.h"
-
+#include "limits.h"
 int main()
 {
-	int a = 0;
  	//ft_printf("%d\n",ft_printf("%%%%%%%s\n%d\n%", 1, 42));
 	//ft_printf("%d\n",ft_printf("%%%%%%%aaa", 42, 7));
-	ft_printf("%x\n", -1);
-	printf("%x\n", -1);
+	ft_printf(" %p  %p\n", ULONG_MAX, LONG_MIN);
+	printf(" %p  %p", ULONG_MAX, LONG_MIN);
 }
